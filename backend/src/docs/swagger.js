0a6accbcef4e1318b28322219b99926eba7cc688
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2026-01-17 08:03:44 -0300

    refatoração: atualiza a documentação da API para usar importações ES6

--- backend/src/docs/swagger.js
+++ backend/src/docs/swagger.js
@@ -1,36 +1,18 @@
-const swaggerJsdoc = require("swagger-jsdoc");
+import swaggerJsdoc from 'swagger-jsdoc';
 
-const swaggerOptions = {
+const swaggerSpec = swaggerJsdoc({
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "API Backend",
-      version: "1.0.0",
-      description: "Documentação da API",
+      title: 'Gestão Patrimônio API',
+      version: '1.0.0',
+      description: 'Documentação da API',
     },
     servers: [
-      {
-        url: "http://localhost:3000",
-      },
+      { url: 'http://localhost:3000' },
     ],
   },
+  apis: ['./src/routes/*.js'],
+});
 
-  apis: ["./src/routes/*.js"], 
-};
-
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-
-module.exports = swaggerSpec;
-
-/*
-public OpenAPI OpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("LUMINA API REST")
-                        .version("1.0.0")
-                        .description("Documentação API REST Lumina")
-                        .contact(new Contact()
-                                .name("Lucca P. Müller")
-                                .email("lumina.unibave@gmail.com")));
-    }
-*/
+export default swaggerSpec;
